641e965a1429dcc4f288c8e99c3950da32fdb989
Bharathhh30 <vnsb30@gmail.com>
2024-10-10 20:11:00 +0530

    added header and hero .. arghhh so much of utility

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './sections/Header'
+import Hero from './sections/hERO.JSX';
+
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+      <Header /> 
+      <Hero />
+    </main>
+  )
+}
+
+export default App;
